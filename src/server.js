40f78fedb4a897a521882cb34bce0792bcdbbd0e
Andra <andra.bogoi@gmail.com>
2021-04-01 18:42:21 +0300

    add messages capabilities to application

--- src/server.js
+++ src/server.js
@@ -1,121 +1,157 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bunyan = require('bunyan');
-const jwt = require('jsonwebtoken');
-const axios = require('axios');
-const logger = bunyan.createLogger({ name: 'conversation-api-function' });
-const path = require('path');
-const { base64encode } = require('nodejs-base64');
-const {generateBEToken,generateUserToken,getStaticConfig} = require('./utils');
-var cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const bunyan = require("bunyan");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const logger = bunyan.createLogger({ name: "conversation-api-function" });
+const path = require("path");
+const { base64encode } = require("nodejs-base64");
+const {
+  generateBEToken,
+  generateUserToken,
+  getStaticConfig,
+} = require("./utils");
+var cors = require("cors");
 
 //const redis = require("redis");
-const StorageClient = require('./storageClient');
-
+const StorageClient = require("./storageClient");
 
 // const conversationApiFunctionModule = require('../hosted_code/server');
 
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
 
 function createExpressApp(config, conversationApiFunctionModule) {
-
-  
-  const app = express()
-  app.use(bodyParser.json())
-  app.use(cors())
-  const {application_id, redis_url} = config
-  const storageClient = new StorageClient({ application_id, redis_url});
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(cors());
+  const { application_id, redis_url } = config;
+  const storageClient = new StorageClient({ application_id, redis_url });
 
   app.use((req, res, next) => {
-    const { query, baseUrl, originalUrl, url, method, statusCode, body } = req
+    const { query, baseUrl, originalUrl, url, method, statusCode, body } = req;
 
     const csClient = async (request) => {
       try {
-        const token = generateBEToken({ config});
+        const token = generateBEToken({ config });
         request.headers = {
-          'Authorization': `Bearer ${token}`
-        }
-        logger.info({request}, "CSClient request -> ")
-        const axiosResponse = await axios(request)
-        logger.info({ request, data: axiosResponse.data ,status: axiosResponse.status   }, "CSClient reponse <-")
-        return axiosResponse
-      }catch(err) {
+          Authorization: `Bearer ${token}`,
+        };
+        logger.info({ request }, "CSClient request -> ");
+        const axiosResponse = await axios(request);
+        logger.info(
+          { request, data: axiosResponse.data, status: axiosResponse.status },
+          "CSClient reponse <-"
+        );
+        return axiosResponse;
+      } catch (err) {
         const requestError = {
-          request: request
-        }
-        if (err.response){
+          request: request,
+        };
+        if (err.response) {
           requestError.response = {
             data: err.response.data,
             status: err.response.status,
             status: err.response.headers,
-          }
-        } 
+          };
+        }
         if (err.message) {
-          requestError.message = err.message
+          requestError.message = err.message;
         }
 
-        logger.error({ ...requestError }, "CSClient error <-")
+        logger.error({ ...requestError }, "CSClient error <-");
         throw err;
       }
-    } 
+    };
     req.nexmo = {
-      generateBEToken: () => generateBEToken({config}), 
-      generateUserToken: (username) => generateUserToken({ config, user_name: username }),
+      generateBEToken: () => generateBEToken({ config }),
+      generateUserToken: (username) =>
+        generateUserToken({ config, user_name: username }),
       logger,
       csClient,
-      storageClient
-    }
-    next()
-  })
-  
-  conversationApiFunctionModule.route(app, express)
+      storageClient,
+    };
+    next();
+  });
 
-  app.get('/ping', (req, res) => res.json({ success: true}))
-  app.post('/voiceEvent', (req, res) => res.json({ body: req.body }))
-  app.post('/rtcEvent', async (req, res) => {
-    const { query, baseUrl, originalUrl, url, method, statusCode, body } = req
-    
-    logger.info({ query, baseUrl, originalUrl, url, method, statusCode, body }, "RTC Event Received <-")
-    res.json({ body: req.body })
+  conversationApiFunctionModule.route(app, express);
 
-    const event = req.body
-    await conversationApiFunctionModule.rtcEvent(event, req.nexmo)
-  })
+  app.get("/ping", (req, res) => res.json({ success: true }));
+  app.post("/voiceEvent", (req, res) => res.json({ body: req.body }));
+  app.post("/rtcEvent", async (req, res) => {
+    const { query, baseUrl, originalUrl, url, method, statusCode, body } = req;
+
+    logger.info(
+      { query, baseUrl, originalUrl, url, method, statusCode, body },
+      "RTC Event Received <-"
+    );
+    res.json({ body: req.body });
+
+    const event = req.body;
+    await conversationApiFunctionModule.rtcEvent(event, req.nexmo);
+  });
+  app.post("/inbound", async (req, res) => {
+    const { query, baseUrl, originalUrl, url, method, statusCode, body } = req;
+
+    logger.info(
+      { query, baseUrl, originalUrl, url, method, statusCode, body },
+      "Messages API Inbound Event Received <-"
+    );
+    res.json({ body: req.body });
+
+    const event = req.body;
+    await conversationApiFunctionModule.messageEvent(event, req.nexmo);
+  });
+  app.post("/status", async (req, res) => {
+    const { query, baseUrl, originalUrl, url, method, statusCode, body } = req;
+
+    logger.info(
+      { query, baseUrl, originalUrl, url, method, statusCode, body },
+      "Messages API Status Event Received <-"
+    );
+    res.json({ body: req.body });
+
+    const event = req.body;
+    await conversationApiFunctionModule.messageEvent(event, req.nexmo);
+  });
 
   return app;
 }
 
-
-
-async function localDevSetup ({ config }) {
-  const { port, application_id, application_name, nexmo_account,isDev, server_url } = config;
+async function localDevSetup({ config }) {
+  const {
+    port,
+    application_id,
+    application_name,
+    nexmo_account,
+    isDev,
+    server_url,
+  } = config;
 
   // if(!isDev)
   // return Promise.resolve({
   //   config
   // });
 
-  const ngrok = require('ngrok');
-  const {api_key, api_secret } = nexmo_account;
-  const dev_api_token = base64encode(`${api_key}:${api_secret}`)
-  logger.info("start application registration")
-  
-  let webhooks_url = server_url 
+  const ngrok = require("ngrok");
+  const { api_key, api_secret } = nexmo_account;
+  const dev_api_token = base64encode(`${api_key}:${api_secret}`);
+  logger.info("start application registration");
 
-  if (!server_url){
-    webhooks_url = await ngrok.connect(port)
-    logger.info("ngrok spinned up locally", { ngrok_url: webhooks_url })
+  let webhooks_url = server_url;
+
+  if (!server_url) {
+    webhooks_url = await ngrok.connect(port);
+    logger.info("ngrok spinned up locally", { ngrok_url: webhooks_url });
   } else {
-    webhooks_url = server_url
+    webhooks_url = server_url;
   }
 
-  const { data, status } =  await axios({
+  const { data, status } = await axios({
     method: "PUT",
     url: `https://api.nexmo.com/v2/applications/${application_id}`,
     data: {
-      "name": application_name,
-      "capabilities": {
+      name: application_name,
+      capabilities: {
         // "voice": {
         //   "webhooks": {
         //     "answer_url": {
@@ -128,80 +164,100 @@ async function localDevSetup ({ config }) {
         //     }
         //   }
         // },
-        "rtc": {
-          "webhooks": {
-            "event_url": {
-              "address": `${webhooks_url}/rtcEvent`,
-              "http_method": "POST"
-            }
-          }
-        }
-      }
+        rtc: {
+          webhooks: {
+            event_url: {
+              address: `${webhooks_url}/rtcEvent`,
+              http_method: "POST",
+            },
+          },
+        },
+        messages: {
+          webhooks: {
+            inbound_url: {
+              address: `${webhooks_url}/inbound`,
+              http_method: "POST",
+            },
+            status_url: {
+              address: `${webhooks_url}/status`,
+              http_method: "POST",
+            },
+          },
+        },
+      },
     },
-    headers: { 'Authorization': `basic ${dev_api_token}` }
-  })
-  
-  logger.info("localDevSetup App Registration*", { data, status, capabilities: data.capabilities })
-      
+    headers: { Authorization: `basic ${dev_api_token}` },
+  });
+
+  logger.info("localDevSetup App Registration*", {
+    data,
+    status,
+    capabilities: data.capabilities,
+  });
+
   return {
     config: {
       ...config,
-      server_url: webhooks_url
-    }
-  }
+      server_url: webhooks_url,
+    },
+  };
 }
 
+function checkEnvVars() {
+  const isDev = !process.env.NODE_ENV;
+  if (isDev) dotenv.config();
 
-function checkEnvVars(){
-  const isDev = !process.env.NODE_ENV
-  if(isDev)
-    dotenv.config();
-
-  let mandatoryEnvs = ["CONV_API_FUNC_PRIVATE_KEY", "CONV_API_FUNC_APPLICATION_ID", "CONV_API_FUNC_APPLICATION_NAME", "CONV_API_FUNC_API_KEY"]
+  let mandatoryEnvs = [
+    "CONV_API_FUNC_PRIVATE_KEY",
+    "CONV_API_FUNC_APPLICATION_ID",
+    "CONV_API_FUNC_APPLICATION_NAME",
+    "CONV_API_FUNC_API_KEY",
+  ];
   // if(isDev)
-  mandatoryEnvs = mandatoryEnvs.concat(["CONV_API_FUNC_API_KEY", "CONV_API_FUNC_API_SECRET"])
-
+  mandatoryEnvs = mandatoryEnvs.concat([
+    "CONV_API_FUNC_API_KEY",
+    "CONV_API_FUNC_API_SECRET",
+  ]);
 
   const getEmpty = (acc, cur) => {
-    if(!process.env[cur]) {
-      return [...acc, cur]
+    if (!process.env[cur]) {
+      return [...acc, cur];
     } else {
-      return acc
+      return acc;
     }
-  }
-  const emptyEnvs = mandatoryEnvs
-  .reduce(getEmpty, [])
+  };
+  const emptyEnvs = mandatoryEnvs.reduce(getEmpty, []);
 
-
-  return emptyEnvs
-
-
+  return emptyEnvs;
 }
-
 
 function listenServer({ app, config }) {
   const { port } = config;
   return new Promise((resolve) => {
-    return app.listen(port, () => resolve({ config }))
-  })
+    return app.listen(port, () => resolve({ config }));
+  });
 }
 
 //
-async function bindLvnToApp({phone_number, application_id, api_key, api_secret}){
+async function bindLvnToApp({
+  phone_number,
+  application_id,
+  api_key,
+  api_secret,
+}) {
   //`msisdn=${phone_number}&voiceCallbackValue=${application_id}&api_key=${api_key}&api_secret=${api_secret}`
-  const reqData = `country=GB&msisdn=${phone_number}&moHttpUrl=&voiceCallbackType=app&voiceCallbackValue=${application_id}&api_key=${api_key}&api_secret=${api_secret}`
-  const {data, status } = await axios({
-        method: "POST",
-        url: `https://rest.nexmo.com/number/update`,
-        data: reqData,
-        headers: { 'Content-Type': `application/x-www-form-urlencoded` }
-      })
-      .catch(err => {
-        logger.error({ err }, "LVN to Application bind failed")
-        throw err;
-      })
+  const reqData = `country=GB&msisdn=${phone_number}&moHttpUrl=&voiceCallbackType=app&voiceCallbackValue=${application_id}&api_key=${api_key}&api_secret=${api_secret}`;
+  const { data, status } = await axios({
+    method: "POST",
+    url: `https://rest.nexmo.com/number/update`,
+    data: reqData,
+    headers: { "Content-Type": `application/x-www-form-urlencoded` },
+  }).catch((err) => {
+    logger.error({ err }, "LVN to Application bind failed");
+    throw err;
+  });
 
-  logger.info({ data, status })
+  logger.info({ data, status });
 }
 
 function startServer(conversationApiFunctionModule) {
@@ -209,27 +265,32 @@ function startServer(conversationApiFunctionModule) {
 
   const emptyEnvs = checkEnvVars();
   if (emptyEnvs.length) {
-    logger.error(`you need to configure the following vars`, emptyEnvs)
-    return Promise.reject(`you need to configure the following vars`, emptyEnvs)
+    logger.error(`you need to configure the following vars`, emptyEnvs);
+    return Promise.reject(
+      `you need to configure the following vars`,
+      emptyEnvs
+    );
   }
 
-  const staticConfig = getStaticConfig(process.env)
-  const {phone_number, application_id, nexmo_account} = staticConfig;
-  const {api_key, api_secret} = nexmo_account;
+  const staticConfig = getStaticConfig(process.env);
+  const { phone_number, application_id, nexmo_account } = staticConfig;
+  const { api_key, api_secret } = nexmo_account;
   return Promise.resolve()
-    .then(() => bindLvnToApp({phone_number, application_id, api_key, api_secret}))
+    .then(() =>
+      bindLvnToApp({ phone_number, application_id, api_key, api_secret })
+    )
     .then(() => localDevSetup({ config: staticConfig }))
     .then(({ config }) => {
-      const app = createExpressApp(config, conversationApiFunctionModule)
-      return { config, app }
+      const app = createExpressApp(config, conversationApiFunctionModule);
+      return { config, app };
     })
     .then(({ app, config }) => listenServer({ app, config }))
     .then(({ config }) => {
       const { port } = config;
-      logger.info(`config`, config)
-      logger.info(`Example app listening on port ${port}!`)
-    })
+      logger.info(`config`, config);
+      logger.info(`Example app listening on port ${port}!`);
+    });
 }
 
-module.exports = startServer
+module.exports = startServer;
 // startServer(conversationApiFunctionModule)
